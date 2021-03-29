ea9810db8044c5825dfc06d2bd0b2a7a9bbd9c92
Andzelika002 <Lisabona794@gmail.com>
2021-03-29 05:42:42 +0300

    Grafines sasaja
    Main, prisijungimas, registracija, paskyros informacija, imones informacija

---
+++ Grafine/Login.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafine
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            this.textBoxPassword.AutoSize = false;
+            this.textBoxPassword.Size = new Size(this.textBoxPassword.Size.Width, 40);
+        }
+
+        private void labelClose_MouseEnter(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.White;
+        }
+
+        private void labelClose_MouseLeave(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.Black;
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void labelRegistration_MouseEnter(object sender, EventArgs e)
+        {
+            labelRegistration.ForeColor = Color.White;
+        }
+
+        private void labelRegistration_MouseLeave(object sender, EventArgs e)
+        {
+            labelRegistration.ForeColor = Color.Black;
+        }
+
+
+    }
+}
---
+++ Grafine/Registration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafine
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+
+        private void labelSingIn_MouseEnter(object sender, EventArgs e)
+        {
+            labelSingIn.ForeColor = Color.White;
+
+        }
+
+        private void labelSingIn_MouseLeave(object sender, EventArgs e)
+        {
+            labelSingIn.ForeColor = Color.Black;
+        }
+
+        private void labelClose_MouseEnter(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.White;
+        }
+
+        private void labelClose_MouseLeave(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.Black;
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+}
